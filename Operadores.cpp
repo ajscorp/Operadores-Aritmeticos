eda55b66db2db4f80a451903273a3a8b0ecb5ad3
Estudiante <estudiante@UCUXA60619.continental.edu.pe>
2023-04-12 17:25:19 -0500

    Aqui realizamos la validacion de ingreso de numeros

--- Operadores.cpp
+++ Operadores.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<string>
 using namespace std;
 
 int main()
@@ -7,35 +8,72 @@ int main()
     setlocale(LC_ALL, "spanish");
 
     //DECLARAR VARIABLES
-    float nro1, nro2, suma, division, multiplicacion, resta;
+    string nro1 = "0", nro2 = "0";
+    int NR1, NR2;
+    float suma, division, multiplicacion, resta;
 
     //TITULO
-    cout << "suma, resta, multiplicación, división, residuo entre 2 números\n" << endl;
-    
-    //INGRESO DE DATOS
-    cout << "Ingrese el primer numero: \t";
-    cin >> nro1;
-    cout << "Ingrese el segundo numero: \t";
-    cin >> nro2;
+    cout << "suma, resta, multiplicaciÃ³n, divisiÃ³n, residuo entre 2 nÃºmeros\n" << endl;
 
-    //PROCESO DE DATOS
-    suma = nro1 + nro2;
-    multiplicacion = nro1 * nro2;
-    if (nro1 < nro2)
+    //INGRESO DE DATOS
+    do
+    {        
+        if (stoi(nro1) <= 0)
+        {
+            cout << "\nIngrese el primer numero: \t";
+            cin >> nro1;
+            for (int i = 0; i < nro1.length(); i++)
+            {
+                if (isdigit(nro1[i]) == false)
+                {
+                    nro1 = "0";
+                    cout << "\nERROR: se tiene que ingresar un numero\n";
+                    system("pause");
+                    break;
+                }
+            }
+        }
+    } while (stoi(nro1) <= 0);
+    do
     {
-        resta = nro2 - nro1;
-        division = nro2 / nro1;
+
+        if (stoi(nro2) <= 0)
+        {
+            cout << "\nIngrese el segundo numero: \t";
+            cin >> nro2;
+            for (int i = 0; i < nro2.length(); i++)
+            {
+                if (isdigit(nro2[i]) == false)
+                {
+                    nro2 = "0";
+                    cout << "\nERROR: se tiene que ingresar un numero\n";
+                    system("pause");
+                    break;
+                }
+            }
+        }
+    } while (stoi(nro2) <= 0);           
+
+    NR1 = stoi(nro1);
+    NR2 = stoi(nro2);
+    //PROCESO DE DATOS
+    suma = NR1 + NR2;
+    multiplicacion = NR1 * NR2;
+    if (NR1 < NR2)
+    {
+        resta = NR2 - NR1;
+        division = NR2 / NR1;
     }
     else
     {
-        resta = nro1 - nro2;
-        division = nro1 / nro2;
+        resta = NR1 - NR2;
+        division = NR1 / NR2;
     }
 
     //SALIDA DE DATOS
     cout << "\n\nLa suma es: \t\t\t\t" << suma << endl;
     cout << "La resta es: \t\t\t\t" << resta << endl;
-    cout << "La multiplicacion es: \t\t" << multiplicacion << endl;
+    cout << "La multiplicacion es: \t\t\t" << multiplicacion << endl;
     cout << "La division es: \t\t\t" << division << endl;
     return 0;
 }
